f306cca5b8fa78d96f8eadfe2e35c81a69bf06c0
sisadminviktor <mivpborisov@mail.ru>
2023-06-26 15:25:29 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+#5.62
+
+# list1 = [5, 6, 7, 8, 9, 10, 3, 4, 5, 6, 7, 8, 9, 9, 5, 4, 4, 5, 6, 8, 8, 9, 9]
+# list2 = [9, 7, 6, 7, 7, 7, 9, 9, 2, 3, 4, 5, 6, 7, 8, 8, 8, 8, 2, 2, 3, 4, 5]
+#
+# resultList1 = (sum(list1) / len(list1))
+# totalList1 = (resultList1)
+# resultList2 = sum(list2) / len(list2)
+# totalList2 = (resultList2)
+#
+# print(f'средний бал первого класса', {totalList1})
+# print(f'средний бал первого второго класса', {totalList2})
+
+#2.43
+
+
+# a = int(input())
+# b = int(input())
+#
+# result1 =  int((a % b == 0) or (b % a == 0))
+# print(result1)
+
+
+# list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+# list.sort()
+# print(list[4])
+
+
+#6.97
+
+# score = [9,7,5,3,1,2,4,6,8,10,11,14]
+# score.sort(reverse=True)
+# place = int(input())
+# result = score.index(place)
+# print(f"набранное количество очков = {place},\nместо команды - {result+1}")
